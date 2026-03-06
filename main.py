ad1503c2aa2050906be7920191e39493bc266b76
Dolgan <dolgan-goryaev@mail.ru>
2026-03-06 13:36:18 +0300

    API3-1 исправления

--- main.py
+++ main.py
@@ -5,12 +5,6 @@ def main():
     map_zoom = 10
     map_ll = [37.977751, 55.757718]
 
-    # строка координат
-    ll = f"{map_ll[0]},{map_ll[1]}"
-
-    # параметры карты
-    params = f"ll={ll}&z={map_zoom}"
-
     # запуск окна Arcade с картой
     show_map(map_ll, add_params=f"z={map_zoom}")
 
