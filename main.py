f0f08d3fe057076a530855fcebfff2ce08e8cec1
Dolgan <dolgan-goryaev@mail.ru>
2026-03-10 09:19:41 +0300

    API-4-1-Погода

--- main.py
+++ main.py
@@ -1,13 +1,49 @@
-from mapapi_Arc import show_map
+import requests
 
+access_key = "8a51d65c-ba34-479e-8a5d-cc347c8e5acb"
 
-def main():
-    map_zoom = 10
-    map_ll = [44.2558, 46.3078]
+headers = {
+    "X-Yandex-Weather-Key": access_key
+}
+lat, lon, q = map(float, input().split())
 
-    # запуск окна Arcade с картой
-    show_map(map_ll, add_params=f"z={map_zoom}")
+query1 = f"""
+    {{
+        weatherByPoint(request: {{ lat: {lat}, lon: {lon} }}) {{
+            now {{
+                humidity
+                pressure
+                temperature
+                windSpeed
+                windDirection
+            }}
+        }}
+    }}"""
 
+response = requests.post('https://api.weather.yandex.ru/graphql/query',
+                         headers=headers, json={'query': query1})
+t1 = response.json()['data']['weatherByPoint']['now']
 
-if __name__ == "__main__":
-    main()
+query2 = f"""
+    {{
+        weatherByPoint(request: {{ lat: {lat}, lon: {lon + q} }}) {{
+            now {{
+                humidity
+                pressure
+                temperature
+                windSpeed
+                windDirection
+            }}
+        }}
+    }}"""
+
+response = requests.post('https://api.weather.yandex.ru/graphql/query',
+                         headers=headers, json={'query': query2})
+t2 = response.json()['data']['weatherByPoint']['now']
+
+params = 'humidity\tpressure\ttemperature\twindSpeed\twindDirection'.split()
+for k in params:
+    print(f'{k}\t{t1[k]}\t{t2[k]}')
+
+# Пример:
+# 44.2558, 46.3078 1.5
