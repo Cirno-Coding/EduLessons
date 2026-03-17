4bd7e550503a866643dba12079ecebecda914e64
Dolgan <dolgan-goryaev@mail.ru>
2026-03-17 11:20:57 +0300

    Web-2-2

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/<title>')
-@app.route('/index/<title>')
-def index(title):
-    return render_template('base.html', title=title)
+@app.route('/training/<prof>')
+def index(prof):
+    return render_template('training.html', prof=prof.lower())
 
 
 if __name__ == '__main__':
