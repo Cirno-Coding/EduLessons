32e739a9392c349fd66b57cd988c7adc8fc6003e
Dolgan <dolgan-goryaev@mail.ru>
2026-04-06 10:34:33 +0300

    asp-4

--- MyFirstWebApp/Pages/Index.cshtml.cs
+++ MyFirstWebApp/Pages/Index.cshtml.cs
@@ -9,21 +9,31 @@ namespace MyFirstWebApp.Pages
         public string Name { get; set; }
         public int Age { get; set; }
         public string Subject { get; set; }
+        public string Email { get; set; }
+        public string ClassName { get; set; }
     }
 
     public class IndexModel : PageModel
     {
+        [BindProperty]
         public string Name { get; set; }
+
+        [BindProperty]
         public int Age { get; set; }
+
+        [BindProperty]
         public string Subject { get; set; }
+
+        [BindProperty]
+        public string Email { get; set; }
+
+        [BindProperty]
+        public string ClassName { get; set; }
         public string Error { get; set; }
 
         public static List<Participant> Participants = new List<Participant>();
         public void OnPostAdd()
         {
-            Name = Request.Form["name"];
-            Subject = Request.Form["subject"];
-
             // Проверка имени
             if (string.IsNullOrEmpty(Name))
             {
@@ -38,14 +48,35 @@ namespace MyFirstWebApp.Pages
                 return;
             }
 
+            if (string.IsNullOrEmpty(Email))
+            {
+                Error = "Введите email";
+                return;
+            }
+
+            if (string.IsNullOrEmpty(ClassName))
+            {
+                Error = "Введите класс";
+                return;
+            }
+
             Age = age;
+
+            if (Age <= 0)
+            {
+                Error = "Возраст должен быть больше 0";
+                return;
+            }
+
 
             // Добавляем в список
             Participants.Add(new Participant
             {
                 Name = Name,
                 Age = Age,
-                Subject = Subject
+                Subject = Subject,
+                Email = Email,
+                ClassName = ClassName
             });
         }
         //Домашка
